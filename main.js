9e68336b408b4c1cd4b25717912f9da64e55685a
renee-coder <71918527+renee-coder@users.noreply.github.com>
2021-01-13 19:36:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,7 +21,22 @@ function setup(){
 function modelLoaded(){
     console.log('Model Loaded!');
 }
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
 
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = "+scoreLeftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+leftWristX+" leftWristY = "+leftWristY)
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+rightWristX+" rightWristY = "+rightWristY)
+    }
+}
 function draw(){
     image(video, 0, 0, 600, 500);
 
@@ -48,19 +63,3 @@ function play(){
     song.rate(1);
 }
 
-function gotPoses(results){
-    if(results.length>0){
-        console.log(results);
-
-        scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist = "+scoreLeftWrist);
-
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = "+leftWristX+" leftWristY = "+leftWristY)
-
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = "+rightWristX+" rightWristY = "+rightWristY)
-    }
-}
