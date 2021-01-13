1456947643fe992609afa4dede19f8c920a00acd
renee-coder <71918527+renee-coder@users.noreply.github.com>
2021-01-13 19:28:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,6 +22,18 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+     
+    if(scoreLeftWrist>0.2){
+       circle(leftWristX,leftWristY,20);
+       InNumberleftWristY = Number(leftWristY);
+       remove_decimals = floor(InNumberleftWristY);
+       volume = remove_decimals/500;
+       document.getElementById("volume").innerHTML="Volume = "+volume;
+       song.setVolume(volume);
+    }
 }
 
 function preload(){
@@ -37,6 +49,10 @@ function play(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = "+scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = "+leftWristX+" leftWristY = "+leftWristY)
