acbe22f68b60b9b5273be95a2e7fefaa7705b09d
renee-coder <71918527+renee-coder@users.noreply.github.com>
2021-01-22 19:36:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ rightWristX = 0;
 rightWristY = 0;
 
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function preload(){
     song = loadSound("music.mp3");
@@ -27,9 +28,9 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-
+        scoreRightWrist = results[0].pose.keypoints[10].score;
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist = "+scoreLeftWrist);
+        console.log("scoreLeftWrist = "+scoreLeftWrist+" scoreRightWrist = "+scoreRightWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -45,7 +46,31 @@ function draw(){
 
     fill("#FF0000");
     stroke("#FF0000");
-     
+
+    if(scoreLeftWrist>0.2){
+    circle(rightWristX, rightWristY, 20);
+
+    if(rightWristY >0 && rightWristY<=100){
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if(rightWristY>100 && rightWristY<=200){
+        document.getElementById("speed").innerHTML = "Speed = 1x";
+        song.rate(1);
+    }
+    else if(rightWristY>200 && rightWristY<=300){
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY>300 && rightWristY<=400){
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2);
+    }
+    else if(rightWristY>400 && rightWristY<=500){
+        document.getElementById("speed").innerHTML = "Speed = 2.5x";
+        song.rate(2.5);
+    }
+     } 
     if(scoreLeftWrist>0.2){
        circle(leftWristX,leftWristY,20);
        InNumberleftWristY = Number(leftWristY);
@@ -54,6 +79,7 @@ function draw(){
        document.getElementById("volume").innerHTML="Volume = "+volume;
        song.setVolume(volume);
     }
+ 
 }
 
 
